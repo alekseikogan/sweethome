adad80c2581b61a3ccd7a7cb9abf4d4c0ccbec1a
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2024-01-31 15:43:51 +0300

    добавил debug_toolbar

--- app_root/app/settings.py
+++ app_root/app/settings.py
@@ -14,6 +14,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -24,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
 
     'main.apps.MainConfig',
     'goods.apps.GoodsConfig',
@@ -37,6 +42,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
