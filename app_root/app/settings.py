862fb813dbd35b002c720cae11eb65ebe9c930b3
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2024-01-31 15:34:35 +0300

    добавил фикстуры и заполнил БД

--- app_root/app/settings.py
+++ app_root/app/settings.py
@@ -103,6 +103,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
     ]
+
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- app_root/main/views.py
+++ app_root/main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+from goods.models import Category, Product
+
 
 def index(request):
     '''Главная страница'''
+    categories = Category.objects.all()
     context = {
         'title': 'Главная страница',
-        'content': 'Главная страница SweetHome'
+        'content': 'Главная страница SweetHome',
+        'categories': categories,
     }
     return render(request, 'main/index.html', context=context)
 
