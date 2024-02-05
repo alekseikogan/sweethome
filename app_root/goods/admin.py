3f09a6816749d38f681f353cf8dead823d379d07
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2024-02-05 15:16:52 +0300

    настроил админ-панель

--- app_root/goods/admin.py
+++ app_root/goods/admin.py
@@ -11,3 +11,16 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'quantity', 'price', 'discount',)
+    list_editable = ('discount',)
+    search_fields = ('name', 'description',)
+    list_filter = ('discount', 'quantity', 'description',)
+    fields = (
+        'name',
+        'category',
+        'slug',
+        'description',
+        'image',
+        ('price', 'discount'),
+        'quantity',
+    )
--- app_root/goods/models.py
+++ app_root/goods/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -67,6 +68,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
 
     def display_id(self):
         return f'{self.id:05}'
