717e688302ec3ad166d2512115234b76baba4492
Алексей <alekseikogan@yandex.ru>
2026-05-09 12:40:24 +0200

    реДизан часть 2

--- app_root/orders/forms.py
+++ app_root/orders/forms.py
@@ -1,5 +1,3 @@
-import re
-
 from django import forms
 
 
@@ -7,7 +5,7 @@ class CreateOrderForm(forms.Form):
 
     first_name = forms.CharField()
     last_name = forms.CharField()
-    phone_number = forms.CharField()
+    phone_number = forms.CharField(max_length=20)
     requires_delivery = forms.ChoiceField(
         choices=[
             ('0', False),
@@ -23,14 +21,4 @@ class CreateOrderForm(forms.Form):
         )
 
     def clean_phone_number(self):
-
-        data = self.cleaned_data['phone_number']
-
-        if not data.isdigit():
-            raise forms.ValidationError("Номер телефона должен содержать только цифры")
-
-        pattern = re.compile(r'^\d{10}$')
-        if not pattern.match(data):
-            raise forms.ValidationError("Неверный формат номера")
-
-        return data
+        return self.cleaned_data['phone_number'].strip()
